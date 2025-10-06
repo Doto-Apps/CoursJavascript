eb42324acae9080c344f462d043eec62eb3401b0
ADominici <dominici.antoine.p@gmail.com>
2025-10-06 15:11:32 +0200

    feat: display info quand reponse trop longue

--- src/components/AnswerForm.tsx
+++ src/components/AnswerForm.tsx
@@ -140,6 +140,12 @@ export const AnswerForm = ({
           answerIndex ? "pointer-events-none" : ""
         }`}
       >
+        {answerIndex===-1? <div role="alert" className="alert alert-error">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  <span>Temps dépassé, réponse anulée..</span>
+</div>:null}
         <legend className="sr-only">
           Selectionner la réponse à la question : {question}
         </legend>
